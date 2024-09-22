53219d614e4fe23375266e70f3501c8d7718de0e
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-09-22 18:12:29 +0100

    feat: finished landing page

--- app.js
+++ app.js
@@ -5,21 +5,25 @@ const serverless = require("serverless-http");
 const router = express.Router();
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Set up logging middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Home route
-router.get('/', (req, res) => {
-  res.sendFile('index.html', { root: './public' });
+router.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "./public" });
+});
+
+router.get("/become_a_partner", (req, res) => {
+  res.sendFile("become_a_partner.html", { root: "./public" });
 });
 
 // Use the router
-app.use('/.netlify/functions/app', router);
+app.use("/.netlify/functions/app", router);
 
 // Logic for testing locally
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
